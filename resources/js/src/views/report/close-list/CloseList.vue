680e9d1f87b6259b0903d2526869eca0f8542056
tunchamroeun <chamroeuntun25@outlook.com>
2020-08-12 15:28:29 +0700

    garage update payment update sale

--- resources/js/src/views/report/close-list/CloseList.vue
+++ resources/js/src/views/report/close-list/CloseList.vue
@@ -1,5 +1,6 @@
 <template>
-    <vx-card no-shadow>
+    <vx-card
+            no-shadow>
         <div class="vx-row">
             <div class="vx-col w-full">
                 <vs-input
@@ -8,7 +9,7 @@
                         placeholder="ជ្រើសរើស"
                         v-model="date_range"
                         class="is-label-placeholder w-full mb-base"
-                        @click="dialog = true" />
+                        @click="dialog = true"/>
                 <q-dialog
                         v-model="dialog"
                         persistent
@@ -30,7 +31,8 @@
                             </q-btn>
                         </q-bar>
 
-                        <q-card-section class="q-px-lg">
+                        <q-card-section
+                                class="q-px-lg">
                             <date-range-picker
                                     :theme="{primary: '#28C76F',secondary: '#2D3E50',ternary: '#93A0BD',border: '#e6e6e6',light: '#ffffff',dark: '#000000',hovers: {day: '#CCC',range: '#e6e6e6'}}"
                                     @update="dateRange"/>
@@ -104,11 +106,16 @@
 </template>
 
 <script>
-    import ShowPurchase from "../../purchase/showPurchase";
+    import ShowPurchase
+        from "../../purchase/showPurchase";
 
-    import StatisticsCardLine from '@/components/statistics-cards/StatisticsCardLine.vue'
-    import MoneyFormat from 'vue-money-format'
-    import ShowInvoice from "../../sale/showInvoice";
+    import StatisticsCardLine
+        from '@/components/statistics-cards/StatisticsCardLine.vue'
+    import MoneyFormat
+        from 'vue-money-format'
+    import ShowInvoice
+        from "../../sale/showInvoice";
+
     export default {
         name: "ReportSale",
         components: {
@@ -119,17 +126,17 @@
         },
         data() {
             return {
-                dialog:false,
-                date_range:`${this.$moment('2019/01/01').format('YYYY-MM-DD')} ~ ${this.$moment().endOf('year').format('YYYY-MM-DD')}`,
-                date_picker:{
-                  start:this.$moment('2019/01/01').format('YYYY-MM-DD'),
-                  end:this.$moment().endOf('year').format('YYYY-MM-DD'),
+                dialog: false,
+                date_range: `${this.$moment('2019/01/01').format('YYYY-MM-DD')} ~ ${this.$moment().endOf('year').format('YYYY-MM-DD')}`,
+                date_picker: {
+                    start: this.$moment('2019/01/01').format('YYYY-MM-DD'),
+                    end: this.$moment().endOf('year').format('YYYY-MM-DD'),
                 }
             }
         },
         computed: {
             //purchase
-            all_investments(){
+            all_investments() {
                 return this.$store.getters.all_investment
             },
             //total investment
@@ -139,38 +146,38 @@
                 }, 0)
             },
             //purchase
-            all_purchases(){
+            all_purchases() {
                 return this.$store.getters.all_purchase
             },
-            all_purchase_received(){
-                let self =  this;
+            all_purchase_received() {
+                let self = this;
                 return self.all_purchases.filter(function (x) {
                     return x.purchase_status === 'received'
                 })
             },
             //filter range date
-            filtered_range_date_purchase(){
+            filtered_range_date_purchase() {
                 let self = this;
                 return self.all_purchase_received.filter(function (x) {
                     let start = new Date(self.date_picker.start);
                     let end = new Date(self.date_picker.end);
                     let date = new Date(x.date);
-                    return date>=start && date<=end;
+                    return date >= start && date <= end;
                 })
             },
             //total due balance
-            total_due_balance_purchase(){
+            total_due_balance_purchase() {
                 let self = this;
-                return self.filtered_range_date_purchase.reduce(function (total,item) {
-                    return total+parseFloat(item.due_balance)
-                },0)
+                return self.filtered_range_date_purchase.reduce(function (total, item) {
+                    return total + parseFloat(item.due_balance)
+                }, 0)
             },
-            total_purchase(){
+            total_purchase() {
                 let self = this;
                 let total = 0;
-                self.filtered_range_date_purchase.forEach(function (item,index) {
-                    item.purchase_detail.forEach(function (el,index) {
-                        if (el.inventory_type ==='inventory_part' || el.inventory_type ==='purchase_only') {
+                self.filtered_range_date_purchase.forEach(function (item, index) {
+                    item.purchase_detail.forEach(function (el, index) {
+                        if (el.inventory_type === 'inventory_part' || el.inventory_type === 'purchase_only') {
                             total += parseFloat(el.amount)
                         }
                     })
@@ -178,42 +185,42 @@
                 return total;
             },
             //end purchase
-            all_invoices(){
+            all_invoices() {
                 return this.$store.getters.all_invoice
             },
-            all_invoice_received(){
-                let self =  this;
+            all_invoice_received() {
+                let self = this;
                 return self.all_invoices.filter(function (x) {
                     return x.invoice_status === 'received'
                 })
             },
             //filter range date
-            filtered_range_date_invoice(){
+            filtered_range_date_invoice() {
                 let self = this;
                 return self.all_invoice_received.filter(function (x) {
                     let start = new Date(self.date_picker.start);
                     let end = new Date(self.date_picker.end);
                     let date = new Date(x.date);
-                    return date>=start && date<=end;
+                    return date >= start && date <= end;
                 })
             },
             //total due balance
-            total_due_balance_invoice(){
+            total_due_balance_invoice() {
                 let self = this;
-                return self.filtered_range_date_invoice.reduce(function (total,item) {
-                    return total+parseFloat(item.due_balance)
-                },0)
+                return self.filtered_range_date_invoice.reduce(function (total, item) {
+                    return total + parseFloat(item.due_balance)
+                }, 0)
             },
-            total_invoice(){
+            total_invoice() {
                 let self = this;
                 let total = 0;
-                self.filtered_range_date_invoice.forEach(function (item,index) {
+                self.filtered_range_date_invoice.forEach(function (item, index) {
                     for (const [index, el] of item.invoice_detail.entries()) {
-                        if (el.inventory_type === 'bundle'){
+                        if (el.inventory_type === 'bundle') {
                             total += parseFloat(item.amount);
                             break;
                         }
-                        if (el.inventory_type !=='purchase_only' && el.inventory_type !=='service') {
+                        if (el.inventory_type !== 'purchase_only') {
                             total += parseFloat(el.amount)
                         }
                     }
@@ -221,68 +228,68 @@
                 return total;
             },
             //expense
-            all_expenses(){
+            all_expenses() {
                 return this.$store.getters.all_expense;
             },
             //filter range date
-            filtered_range_date_expense(){
+            filtered_range_date_expense() {
                 let self = this;
                 return self.all_expenses.filter(function (x) {
                     let start = new Date(self.date_picker.start);
                     let end = new Date(self.date_picker.end);
                     let date = new Date(x.date);
-                    return date>=start && date<=end;
+                    return date >= start && date <= end;
                 })
             },
-            _total_expense(){
+            _total_expense() {
                 self = this;
                 let total = 0;
-                self.filtered_range_date_expense.forEach(function (item,index) {
-                    total+=parseFloat(self.total_expense(item.expense_detail))
+                self.filtered_range_date_expense.forEach(function (item, index) {
+                    total += parseFloat(self.total_expense(item.expense_detail))
                 });
                 return total;
             },
             //income
-            all_incomes(){
+            all_incomes() {
                 return this.$store.getters.all_income;
             },
             //filter range date
-            filtered_range_date(){
+            filtered_range_date() {
                 let self = this;
                 return self.all_incomes.filter(function (x) {
                     let start = new Date(self.date_picker.start);
                     let end = new Date(self.date_picker.end);
                     let date = new Date(x.date);
-                    return date>=start && date<=end;
+                    return date >= start && date <= end;
                 })
             },
-            _total_income(){
+            _total_income() {
                 self = this;
                 let total = 0;
-                self.filtered_range_date.forEach(function (item,index) {
-                    total+=parseFloat(self.total_income(item.income_detail))
+                self.filtered_range_date.forEach(function (item, index) {
+                    total += parseFloat(self.total_income(item.income_detail))
                 });
                 return total;
             },
             //PAYROLL
-            all_close_payrolls(){
+            all_close_payrolls() {
                 return this.$store.getters.all_report_close_payroll;
             },
             //filter range date
-            filtered_range_date_close_payroll(){
+            filtered_range_date_close_payroll() {
                 let self = this;
                 return self.all_close_payrolls.filter(function (x) {
                     let start = new Date(self.date_picker.start);
                     let end = new Date(self.date_picker.end);
                     let date = new Date(x.date);
-                    return date>=start && date<=end;
+                    return date >= start && date <= end;
                 })
             },
-            _total_close_payroll(){
+            _total_close_payroll() {
                 self = this;
-                return self.filtered_range_date_close_payroll.reduce(function (total,item) {
-                    return total+parseFloat(item.salary)
-                },0)
+                return self.filtered_range_date_close_payroll.reduce(function (total, item) {
+                    return total + parseFloat(item.salary)
+                }, 0)
             }
         },
         methods: {
@@ -294,16 +301,16 @@
                 this.date_picker.end = this.$moment(value.to).format('YYYY-MM-DD');
             },
             //calc total expense
-            total_expense(data){
-                return data.reduce(function (total,item) {
-                    return total+parseFloat(item.balance)
-                },0)
+            total_expense(data) {
+                return data.reduce(function (total, item) {
+                    return total + parseFloat(item.balance)
+                }, 0)
             },
             //calc total income
-            total_income(data){
-                return data.reduce(function (total,item) {
-                    return total+parseFloat(item.balance)
-                },0)
+            total_income(data) {
+                return data.reduce(function (total, item) {
+                    return total + parseFloat(item.balance)
+                }, 0)
             },
         }
     }
